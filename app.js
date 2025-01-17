2783adb4a4e6826ef619feda5243b666efc813ac
farah001400 <farah001400@gmail.com>
2025-01-17 22:46:46 +0500

    some changings

--- app.js
+++ app.js
@@ -56,30 +56,33 @@
 //     document.getElementById('city').value = cityName;
 // }
 //---------------------------reading and setting paragraph text(51)-----------------------//
-// function readParagraphText(){
-//     var initialpara = document.getElementById('initial_para');
-//     var outputpara = document.getElementById('output_para');
-//     outputpara.textContent = 'Current paragraph text:' + '"' + initialpara.textContent+ '"'
-// }
-// function setParagraphText(){
-//     var initialpara = document.getElementById('initial_para');
-//     var newText = document.getElementById('newTextInput');
-//     var outputpara = document.getElementById('output_para');
-//     if (newText.value.trim() === '') {
-//         outputpara.textContent = "Please enter some text to set!";
-//         outputpara.style.color = "red";
-//     }
-//     else {
-//         initialpara.textContent = newText.value;
-//         outputpara.textContent = "Paragraph text has been updated!";
-//         outputpara.style.color = "green";
-//         newText.value = "";
-//     }
-// }
-// OR //
-function expandPara() {
-    var expandParagraph = document.getElementById('abc').textContent;
-    document.getElementById('slowLoris').innerHTML = expandParagraph;
+function readParagraphText(){
+    var initialpara = document.getElementById('initial_para');
+    var outputpara = document.getElementById('output_para');
+    outputpara.textContent = 'Current paragraph text:' + '"' + initialpara.textContent+ '"'
 }
+function setParagraphText(){
+    var initialpara = document.getElementById('initial_para');
+    var newText = document.getElementById('newTextInput');
+    var outputpara = document.getElementById('output_para');
+    if (newText.value.trim() === '') {
+        outputpara.textContent = "Please enter some text to set!";
+        outputpara.style.color = "red";
+    }
+    else {
+        initialpara.textContent = newText.value;
+        outputpara.textContent = "Paragraph text has been updated!";
+        outputpara.style.color = "green";
+        newText.value = "";
+    }
+}
+// OR //
+function expandParagraph(){
+    var more = document.getElementById('morePara');
+    var less = document.getElementById('lessPara');
+    less.innerHTML = more.innerHTML;
+}
+
+
 
 
