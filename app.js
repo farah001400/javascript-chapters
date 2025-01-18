8bafd9e9fbc38c3160aa653e42858f84941c9c73
farah001400 <farah001400@gmail.com>
2025-01-18 21:51:05 +0500

    changings

--- app.js
+++ app.js
@@ -56,59 +56,77 @@
 //     document.getElementById('city').value = cityName;
 // }
 //---------------------------reading and setting paragraph text(51)-----------------------//
-function readParagraphText() {
-    var initialpara = document.getElementById('initial_para');
-    var outputpara = document.getElementById('output_para');
-    outputpara.textContent = 'Current paragraph text:' + '"' + initialpara.textContent + '"'
-}
-function setParagraphText() {
-    var initialpara = document.getElementById('initial_para');
-    var newText = document.getElementById('newTextInput');
-    var outputpara = document.getElementById('output_para');
-    if (newText.value.trim() === '') {
-        outputpara.textContent = "Please enter some text to set!";
-        outputpara.style.color = "red";
-    }
-    else {
-        initialpara.textContent = newText.value;
-        outputpara.textContent = "Paragraph text has been updated!";
-        outputpara.style.color = "green";
-        newText.value = "";
-    }
-}
-// OR //
-function expandParagraph() {
-    var more = document.getElementById('morePara');
-    var less = document.getElementById('lessPara');
-    less.innerHTML = more.innerHTML;
-}
-function placeAList() {
-    var listToPlace = "<ul><li>Slow loris</li><li>Fast loris</li><li>Just-right loris</li></ul>";
-    var seeList = document.getElementById('seeList');
-    seeList.innerHTML = listToPlace;
-    document.getElementById('list').style.display = 'none';
-}
+// function readParagraphText() {
+//     var initialpara = document.getElementById('initial_para');
+//     var outputpara = document.getElementById('output_para');
+//     outputpara.textContent = 'Current paragraph text:' + '"' + initialpara.textContent + '"'
+// }
+// function setParagraphText() {
+//     var initialpara = document.getElementById('initial_para');
+//     var newText = document.getElementById('newTextInput');
+//     var outputpara = document.getElementById('output_para');
+//     if (newText.value.trim() === '') {
+//         outputpara.textContent = "Please enter some text to set!";
+//         outputpara.style.color = "red";
+//     }
+//     else {
+//         initialpara.textContent = newText.value;
+//         outputpara.textContent = "Paragraph text has been updated!";
+//         outputpara.style.color = "green";
+//         newText.value = "";
+//     }
+// }
+// // OR //
+// function expandParagraph() {
+//     var more = document.getElementById('morePara');
+//     var less = document.getElementById('lessPara');
+//     less.innerHTML = more.innerHTML;
+// }
+// function placeAList() {
+//     var listToPlace = "<ul><li>Slow loris</li><li>Fast loris</li><li>Just-right loris</li></ul>";
+//     var seeList = document.getElementById('seeList');
+//     seeList.innerHTML = listToPlace;
+//     document.getElementById('list').style.display = 'none';
+// }
 //--------------------------------manipulating images and text(52)--------------------------//
-function toggleImage() {
-    if (image.src.includes("assets/dog.jpeg")) {
-        image.src = "assets/horse.jpg";
-        outputPara.textContent = "Image has been changed to the first image!"
-        outputPara.style.color = "green";
-        outputPara.style.fontWeight = "bold";
-    }
-    else {
-        image.src = "assets/dog.jpeg";
-        outputPara.textContent = "Image has been changed to the second image!"
-    }
+var paragraph = document.getElementById("paragraph");
+var input = document.getElementById("input");
+var outputPara = document.getElementById("outputPara");
+var image = document.getElementById("image");
+function setParagraph() {
+    if(input.value.trim() === ""){
+    outputPara.textContent = "Please enter some text!"
+    outputPara.style.color = "red";
+    outputPara.style.fontWeight= "bold"
 }
-function hideImage() {
-    image.style.display = "none";
-    outputPara.textContent = "image is now hiden!"
+else{
+    paragraph.textContent = input.value;
+    input.value = "";
+    outputPara.textContent = "New paragrapg has been updated!"
     outputPara.style.color = "green";
+    outputPara.style.fontWeight = "bold";
+}
+}
+function toggleImage() {
+if(image.src.includes("assets/dog.jpeg")){
+    image.src = "assets/horse.jpg";
+    outputPara.textContent = "Image has been changed to the first image!"
+    outputPara.style.color = "green";
+    outputPara.style.fontWeight = "bold";
+}
+else{
+    image.src = "assets/dog.jpeg";
+    outputPara.textContent = "Image has been changed to the second image!"
+}
+}
+function hideImage(){
+image.style.display = "none";
+outputPara.textContent = "image is now hiden!"
+outputPara.style.color = "green";
 }
 function showImage() {
-    image.style.display = "inline";
-    outputPara.textContent = "image is now visible!"
+image.style.display = "inline";
+outputPara.textContent = "image is now visible!"
 }
 
 
