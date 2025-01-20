bd6d5dea7beb915d95a241a1a201663808f67143
farah001400 <farah001400@gmail.com>
2025-01-20 18:23:28 +0500

    one more function

--- (53) swapping images/app.js
+++ (53) swapping images/app.js
@@ -2,3 +2,8 @@ function swapImage(imageSrc){
     var selectedImage = document.getElementById('selectedImage');
      selectedImage.src = imageSrc
 }
+
+function swapPic(){
+    var before = document.getElementById('before');
+    before.src = "assets/nature2.jpg";
+}
