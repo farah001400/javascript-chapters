a17f7ad16a5dfff669d190eed220c5e51892b2f6
farah001400 <farah001400@gmail.com>
2025-01-22 15:16:15 +0500

    practice the same function

--- (54)swapping images and setting classes/app.js
+++ (54)swapping images and setting classes/app.js
@@ -9,3 +9,22 @@ function swapImage(newImgSrc, clickedThumbnail) {
     mainImg.style.border = "1px solid blue"
 }
 
+// function swapImage(imgSrc, element){
+//     var mainImg = document.getElementById('mainImage');
+//     mainImg.src = imgSrc;
+//     var elements = document.querySelectorAll('.gallery img');
+//     for(var i = 0; i < elements.length; i++){
+//         elements[i].classList.remove('active')    
+//     }
+//     element.classList.add('active');
+// }
+
+// function swapImage(currentImgSrc, ele){
+//     var mainImage = document.getElementById('mainImage');
+//     mainImage.src = currentImgSrc;
+//     var allElements = document.querySelectorAll('.gallery img');
+//     for(var i = 0; i < allElements.length; i++){
+//         allElements[i].classList.remove('active');
+//         }
+//     ele.classList.add('active');
+// }
