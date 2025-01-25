9553d57fd679d167a7611661a114b49ea0041cfd
farah001400 <farah001400@gmail.com>
2025-01-25 18:13:30 +0500

    56 chap html & css

---
+++ (56)target all elements by tag name/app.js
@@ -0,0 +1,5 @@
+// function styleAllParagraphs(){
+//     var allPara = document.getElementsByTagName('p');
+//     allPara.style.backgroundColor = 'green';
+//     allPara.style.color = "white";
+// }
