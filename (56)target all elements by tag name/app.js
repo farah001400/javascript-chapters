b1440ff04bdbbe8754091ae14bdd4bd2d3c7da93
farah001400 <farah001400@gmail.com>
2025-01-25 21:39:56 +0500

    made a function

--- (56)target all elements by tag name/app.js
+++ (56)target all elements by tag name/app.js
@@ -1,5 +1,7 @@
-// function styleAllParagraphs(){
-//     var allPara = document.getElementsByTagName('p');
-//     allPara.style.backgroundColor = 'green';
-//     allPara.style.color = "white";
-// }
+function styleAllParagraphs(){
+    var paragraphs = document.getElementsByTagName('p');
+    for(var i = 0; i < paragraphs.length; i++){
+        paragraphs[i].classList.add('styled')
+    }
+}
+
