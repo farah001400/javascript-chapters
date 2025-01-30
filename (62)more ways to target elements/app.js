644ad2199d0247971c761e0bbddeb87173a5162e
farah001400 <farah001400@gmail.com>
2025-01-31 00:22:57 +0500

    practiced with one mor function

--- (62)more ways to target elements/app.js
+++ (62)more ways to target elements/app.js
@@ -1,25 +1,67 @@
 var myList = document.getElementById('myList');
-var firstItem = myList.firstElementChild;
-var lastItem = myList.lastElementChild;
-var current = firstItem;
+var itemfirst = myList.firstElementChild;
+var itemlast = myList.lastElementChild;
+var currentItem = itemfirst;
 
 function highLightFirst() {
-    current.style.backgroundColor = "";
-    current = firstItem;
-    current.style.backgroundColor = "green";
-    current.style.color = 'white';
+    currentItem.style.backgroundColor = "";
+    currentItem = itemfirst;
+    currentItem.style.backgroundColor = "green";
+    currentItem.style.color = 'white';
 }
 function highLightLast() {
-    current.style.backgroundColor = "";
-    current = lastItem;
-    current.style.backgroundColor = "green";
-    current.style.color = "white";
+    currentItem.style.backgroundColor = "";
+    currentItem = itemlast;
+    currentItem.style.backgroundColor = "green";
+    currentItem.style.color = "white";
 }
 function highLightNext() {
-    current.style.backgroundColor = "";
-    if (current.nextElementSibling) {
-        current = current.nextElementSibling;
+    currentItem.style.backgroundColor = "";
+    if (currentItem.nextElementSibling) {
+        currentItem = currentItem.nextElementSibling;
     }
-    current.style.backgroundColor = "green";
+    currentItem.style.backgroundColor = "green";
+    currentItem.style.color = "white";
+}
+//---------------------------------------------------------------------------------------------------------------------------------//
+
+var list = document.getElementById('parent');
+var noItem =document.getElementById('noItem');
+var firstItem = list.firstElementChild;
+var lastItem = list.lastElementChild;
+var current = firstItem;
+console.log(firstItem);
+
+function firstHighLight(){
+    noItem.style.display = "none";
+    current.style.backgroundColor = "";
+    current.style.color = 'black';
+    current = firstItem;
+    current.style.backgroundColor = 'blue';
     current.style.color = "white";
+}
+function lastHighLight(){
+    noItem.style.display = "none";
+    current.style.backgroundColor = "";
+    current.style.color ='black';
+    current = lastItem;
+    current.style.backgroundColor = "blue";
+    current.style.color = "white";
+}
+
+function nextHighLight(){
+    current.style.backgroundColor = "";
+    current.style.color = 'black'
+    if(current.nextElementSibling){
+        current = current.nextElementSibling;
+        current.style.backgroundColor = "blue";
+        current.style.color = 'white';
+    }
+    else{
+         noItem.style.display = "block";
+    noItem.innerText = "No more items";
+    noItem.style.color = 'red';
+    noItem.style.fontWeight = "bold";
+    noItem.style.paddingBottom = "10px "
+    }   
 }
