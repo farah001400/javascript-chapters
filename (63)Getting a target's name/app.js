a69a4b1c47e2d460e43f2256cd07bcc7d03c69f8
farah001400 <farah001400@gmail.com>
2025-02-01 22:01:21 +0500

    practice

--- (63)Getting a target's name/app.js
+++ (63)Getting a target's name/app.js
@@ -11,3 +11,18 @@ var clickableElements = document.querySelectorAll('.clickable');
 for(var i = 0; i < clickableElements.length; i++){
     clickableElements[i].addEventListener('click', displayElementInfo)
 }
+
+//-----------------------------------------------Practice the same function---------------------------------------------------------------//
+var infoPanel = document.getElementById('infoPanel');
+var nodeNameElement = document.getElementById('nodeName');
+var nodeValueElement = document.getElementById('nodeValue');
+
+function displayElementInfo(event){
+    var element = event.target;
+    nodeNameElement.textContent = 'Node Name:' + element.nodeName;
+    nodeValueElement.textContent = 'Node Value:' + (element.firstChild ? element.firstChild.nodeValue : 'null');
+}
+var clickableElements = document.querySelectorAll('.clickable');
+for(var i = 0; i < clickableElements.length; i++){
+    clickableElements[i].addEventListener('click', displayElementInfo)
+}
