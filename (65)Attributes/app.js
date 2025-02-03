a5926a95eebcdc2f49ef4670e238990c8f0323c8
farah001400 <farah001400@gmail.com>
2025-02-04 00:22:11 +0500

    practiced the sme function

--- (65)Attributes/app.js
+++ (65)Attributes/app.js
@@ -9,3 +9,33 @@ function changeAttributes(){
     if(newTitle) image.title = newTitle;
     console.log(image);
 }
+//------------------------------------practiced the same function-------------------------------------------------------//
+// function changeAttributes(){
+//     var image = document.getElementById('myImage');
+//     var newSrc = document.getElementById('srcInput').value;
+//     var newAlt = document.getElementById('altInput').value;
+//     var newTitle = document.getElementById('titleInput').value;
+//     // console.log(image, newSrc, newAlt, newTitle);
+//     if(newSrc) {
+//         image.src = newSrc;
+//     }
+//     if(newAlt){
+//         image.alt = newAlt;
+//     }
+//     if(newTitle){
+//         image.title = newTitle;
+//     }
+//     console.log(image);
+// }
+//-----------------------------------------practiced the same function--------------------------------------------------//
+// var attributes = document.getElementById('attri');
+// attributes.onclick = function(){
+//     var image = document.getElementById('myImage');
+//     var newSrc = document.getElementById('srcInput').value;
+//     var newAlt = document.getElementById('altInput').value;
+//     var newTitle = document.getElementById('titleInput').value;
+//     console.log(image, newSrc, newAlt, newTitle);
+//     if(newSrc) image.src = newSrc;
+//     if(newAlt) image.alt = newAlt;
+//     if(newTitle) image.title = newTitle;
+// }
