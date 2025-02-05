0fd0486f33a8d9ab9079a1b79f3d7830981a619c
farah001400 <farah001400@gmail.com>
2025-02-05 20:31:56 +0500

    javascript

---
+++ (67)adding nodes/app.js
@@ -0,0 +1,35 @@
+function addTask(){
+   var taskInput = document.getElementById('taskInput');
+   var taskList = document.getElementById('taskList');
+
+   if(taskInput.value.trim() === ""){
+    return alert('Please enter a task!');
+   }
+
+   // Create a new list item //
+   var li = document.createElement('li');
+   li.textContent = taskInput.value;
+
+   // Create a remove button //
+   var removeButton = document.createElement('button');
+   removeButton.textContent = "Remove";
+   removeButton.className = "remove-btn"
+   removeButton.onclick = function(){
+    taskList.removeChild(li);
+   }
+   // Add click eventto mark as completed
+   li.onclick = function (){
+    li.classList.toggle('completed');
+   }
+
+   // Append the remove button to the list item //
+   li.appendChild(removeButton);
+
+   //Append the list item to the task list //
+   taskList.appendChild(li);
+
+   //Clear the input field //
+   taskInput.value = "";
+
+//    console.log(removeButton);
+}
