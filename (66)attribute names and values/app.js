264d6e9cad71e89d52e24b927242734a0f9db3c4
farah001400 <farah001400@gmail.com>
2025-02-07 00:36:16 +0500

    made a function

--- (66)attribute names and values/app.js
+++ (66)attribute names and values/app.js
@@ -1,3 +1,12 @@
 function updateProfile(imgSrc, name, age, profession) {
+    var rightSide = document.getElementById('rightSideContent')
+    var profileImg = document.getElementById('profileImg');
+    profileImg.setAttribute('src', imgSrc);
+    rightSide.setAttribute('data-name', name);
+    rightSide.setAttribute('data-age', age);
+    rightSide.setAttribute('data-profession', profession);
 
+    document.getElementById('name').textContent = rightSide.getAttribute('data-name');
+    document.getElementById('age').textContent = 'Age: ' + rightSide.getAttribute('data-age');
+    document.getElementById('profession').textContent = 'Profession: ' + rightSide.getAttribute('data-profession');
 }
