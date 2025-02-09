8e62c7631f7e728d007ac7fc48f64338ce9f7e52
farah001400 <farah001400@gmail.com>
2025-02-09 23:25:03 +0500

    part2 of the same function

--- (68)inserting nodes/app.js
+++ (68)inserting nodes/app.js
@@ -1,9 +1,9 @@
+//----part-1----//
 function createListItem(text) {
     var li = document.createElement('li');
     li.textContent = text;
     return li;
 }
-
 function addMainItem() {
     var text = document.getElementById('itemInput').value;
     if(text) {
@@ -16,9 +16,7 @@ function addMainItem() {
     }
 }
 
-
-
-
+//----part-2----//
 var activeItem = null;
 document.getElementById('list').addEventListener('click', handleListClick);
 
@@ -34,8 +32,35 @@ function handleListClick(e) {
 }
 
 function addSubItem() {
-
+    if(activeItem) {
+        var text = document.getElementById('itemInput').value;
+        if(text) {
+            var subUl = activeItem.querySelector('ul') || document.createElement('ul');
+            var li = createListItem(text);
+            subUl.appendChild(li);
+            if(!activeItem.querySelector('ul')) {
+                activeItem.appendChild(subUl);
+            }
+            document.getElementById('itemInput').value = "";
+        }
+    }
+    else{
+        alert('Please select an item first.')
+    }
 }
+//----part-3----//
+
+
+
+
+
+
+
+
+
+
+
+
 // ------------------------------------------------------------------------------------------------//
 
 // function addMainItem() {
